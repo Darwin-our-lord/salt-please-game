3c5039a6f09ed60218a4d83e7f5b4d6b0b145f1e
Darwin-our-lord <David@prebensen.dk>
2026-02-26 20:51:53 +0100

    fuck that

--- SALT PLEASE/Assets/Scripts/SaltSpawner.cs
+++ SALT PLEASE/Assets/Scripts/SaltSpawner.cs
@@ -9,6 +9,5 @@ public class SaltSpawner : MonoBehaviour
         GameObject slat =Instantiate(saltOBJ,transform.position,Quaternion.identity,transform);
         slat.GetComponent<SaltOBJ>().isDragging = true;
         slat.GetComponent<SaltOBJ>().salt = gameManager.currentSalt;
-        Debug.Log(slat.GetComponent<SaltOBJ>().salt);
     }
 }
